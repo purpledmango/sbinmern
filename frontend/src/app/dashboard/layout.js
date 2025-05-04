5e8024c07edc10679742c86e278cafdb87abae8b
purpledmango <nitishmerlin@gmail.com>
2025-05-04 10:55:49 +0530

    build chnages

---
+++ frontend/src/app/dashboard/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Dashboard- HostAstra.com",
+  description: "Presenting HostAstra.com a solution for Wordpress and Manento handcrafed by ProfPresenting HostAstra.com: a solution for WordPress and Magento, handcrafted by professionals.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
